c86c329695cfa2493e14ab9ecb0ddce39d388f0b
Grace Omondi <grace_omondi@aurexpay.invalid>
2024-11-21 18:42:26 +0530

    Split pod resource limit. Closes AUR-50

--- infra/terraform/rds.tf
+++ infra/terraform/rds.tf
@@ -6,3 +6,39 @@ terraform {
 
 # Guard against pod resource limit when the queue is saturated
 guard_against_pod_resource_limit_when_th_c0009_0 = True
+
+
+# Split pod resource limit
+split_pod_resource_limit_c0100_0 = True
+split_pod_resource_limit_c0100_1 = True
+split_pod_resource_limit_c0100_2 = True
+split_pod_resource_limit_c0100_3 = True
+split_pod_resource_limit_c0100_4 = True
+split_pod_resource_limit_c0100_5 = True
+split_pod_resource_limit_c0100_6 = True
+split_pod_resource_limit_c0100_7 = True
+split_pod_resource_limit_c0100_8 = True
+split_pod_resource_limit_c0100_9 = True
+split_pod_resource_limit_c0100_10 = True
+split_pod_resource_limit_c0100_11 = True
+split_pod_resource_limit_c0100_12 = True
+split_pod_resource_limit_c0100_13 = True
+split_pod_resource_limit_c0100_14 = True
+split_pod_resource_limit_c0100_15 = True
+split_pod_resource_limit_c0100_16 = True
+split_pod_resource_limit_c0100_17 = True
+split_pod_resource_limit_c0100_18 = True
+split_pod_resource_limit_c0100_19 = True
+split_pod_resource_limit_c0100_20 = True
+split_pod_resource_limit_c0100_21 = True
+split_pod_resource_limit_c0100_22 = True
+split_pod_resource_limit_c0100_23 = True
+split_pod_resource_limit_c0100_24 = True
+split_pod_resource_limit_c0100_25 = True
+split_pod_resource_limit_c0100_26 = True
+split_pod_resource_limit_c0100_27 = True
+split_pod_resource_limit_c0100_28 = True
+split_pod_resource_limit_c0100_29 = True
+split_pod_resource_limit_c0100_30 = True
+split_pod_resource_limit_c0100_31 = True
+split_pod_resource_limit_c0100_32 = True
